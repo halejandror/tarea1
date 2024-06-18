b5df099adc9a69fbc5b388f79b43afc1af0ecbfb
ALEJANDRORODRIGUEZ <usuariolg80@gmail.com>
2024-06-17 22:50:37 -0500

    primer tarea modulo 3

---
+++ alejandro-rodriguez/main.js
@@ -0,0 +1,8 @@
+
+function cubicar(a,b,c,d) {
+    let porcetajeFacturar= (a * b / 10000 * c / d);
+    
+    console.log(porcetajeFacturar.toFixed(2))
+  }
+  cubicar(31,60,4,1.68);
+  cubicar(51,51,2,2.34);
